e44ad7f7efe30075aff8c0494d2e683314a4207e
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-06-15 00:00:35 +0530

    Initial Commit

---
+++ Listen/ListenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenApp.swift
+//  Listen
+//
+//  Created by Hitesh Madaan on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ListenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
