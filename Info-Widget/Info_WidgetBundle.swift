09845496e18a73279c0b7d1c6f34a22cede24c07
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-07-06 12:14:46 +0530

    Added a Widget Functionlity to the application

---
+++ Info-Widget/Info_WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Info_WidgetBundle.swift
+//  Info-Widget
+//
+//  Created by Hitesh Madaan on 06/07/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Info_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Info_Widget()
+        Info_WidgetControl()
+    }
+}
